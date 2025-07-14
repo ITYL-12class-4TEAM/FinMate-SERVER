f9cd4ecb93aac9e4fc6b67937e3ae0189484e0d1
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-07-14 14:24:49 +0900

    feat: 초기 설정 및 서브모듈 설정

---
+++ src/main/java/org/scoula/common/config/RootConfig.java
@@ -0,0 +1,142 @@
+package org.scoula.common.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@PropertySource("file:${config.location}/application.properties")
+//@MapperScan(basePackages = {"org.scoula.mapper"}) // Mapper 인터페이스 스캔 설정
+public class RootConfig {
+
+  @Value("${jdbc.driver}")
+  String driver;
+  @Value("${jdbc.url}")
+  String url;
+  @Value("${jdbc.username}")
+  String username;
+  @Value("${jdbc.password}")
+  String password;
+  @Autowired
+  ApplicationContext applicationContext;
+  @PostConstruct
+  public void printProperties() {
+    System.out.println("[TEST] jdbc.driver = " + driver);
+    System.out.println("[TEST] jdbc.url = " + url);
+    System.out.println("[TEST] jdbc.username = " + username);
+    System.out.println("[TEST] jdbc.password = " + password);
+  }
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertyConfig() throws IOException {
+    PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+
+    String configLocation = System.getProperty("config.location");
+    if (configLocation == null) {
+      throw new IllegalStateException("System property 'config.location' must be set");
+    }
+
+    System.out.println("[DEBUG] config.location: " + configLocation);
+    System.out.println("[DEBUG] user.dir: " + System.getProperty("user.dir"));
+
+    List<Resource> resources = new ArrayList<>();
+    File baseFile = new File(configLocation + "/application.properties");
+    if (baseFile.exists()) {
+      resources.add(new FileSystemResource(baseFile));
+    }
+
+    // application.properties에서 active.profile을 직접 읽어옴
+    Properties baseProps = new Properties();
+    baseProps.load(new FileInputStream(baseFile));
+    String activeProfiles = baseProps.getProperty("active.profile", "local");
+
+    for (String profile : activeProfiles.split("\\s*,\\s*")) {
+      String profilePath = configLocation + "/application-" + profile + ".properties";
+      File profileFile = new File(profilePath);
+      if (profileFile.exists()) {
+        System.out.println("[INFO] Load profile config: " + profilePath);
+        resources.add(new FileSystemResource(profileFile));
+      } else {
+        System.err.println("[WARN] Profile config not found: " + profilePath);
+      }
+    }
+
+    // 최종 설정
+    configurer.setLocations(resources.toArray(new Resource[0]));
+    configurer.setIgnoreUnresolvablePlaceholders(true);
+
+    return configurer;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    // HikariCP 설정 객체 생성
+    HikariConfig config = new HikariConfig();
+
+    // 데이터베이스 연결 정보 설정
+    config.setDriverClassName(driver);          // JDBC 드라이버 클래스
+    config.setJdbcUrl(url);                    // 데이터베이스 URL
+    config.setUsername(username);              // 사용자명
+    config.setPassword(password);              // 비밀번호
+
+    // 커넥션 풀 추가 설정 (선택사항)
+    config.setMaximumPoolSize(10);             // 최대 커넥션 수
+    config.setMinimumIdle(5);                  // 최소 유지 커넥션 수
+    config.setConnectionTimeout(30000);       // 연결 타임아웃 (30초)
+    config.setIdleTimeout(600000);            // 유휴 타임아웃 (10분)
+
+    // HikariDataSource 생성 및 반환
+    HikariDataSource dataSource = new HikariDataSource(config);
+    return dataSource;
+  }
+
+  /**
+   * SqlSessionFactory 빈 등록
+   * - MyBatis의 핵심 팩토리 객체를 스프링 컨테이너에 등록
+   *
+   * @param dataSource 위 dataSource() 메서드에서 등록된 bean이 주입됨
+   */
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+
+    // MyBatis 설정 파일 위치 지정
+    sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
+
+    // 데이터베이스 연결 설정
+    sqlSessionFactory.setDataSource(dataSource);
+
+    return sqlSessionFactory.getObject();
+  }
+
+  /**
+   * 트랜잭션 매니저 설정
+   * - 데이터베이스 트랜잭션을 스프링이 관리하도록 설정
+   */
+  @Bean
+  public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+    DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource);
+    return manager;
+  }
+}
---
+++ src/main/java/org/scoula/common/config/ServletConfig.java
@@ -0,0 +1,68 @@
+package org.scoula.common.config;
+
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "org.scoula.exception",
+        "org.scoula.controller"
+}) // Spring MVC용 컴포넌트 등록을 위한 스캔 패키지
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 기본 리소스 설정
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+
+        // Swagger UI 리소스를 위한 핸들러 설정
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        // Swagger WebJar 리소스 설정 (Bootstrap, jQuery 등)
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // Swagger 메타데이터 리소스 설정
+        registry.addResourceHandler("/swagger-resources/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        // API 문서 JSON 엔드포인트 설정
+        registry.addResourceHandler("/v2/api-docs")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);           // JSTL 지원 활성화
+        bean.setPrefix("/WEB-INF/views/");           // JSP 파일 기본 경로
+        bean.setSuffix(".jsp");                      // JSP 파일 확장자
+        registry.viewResolver(bean);                 // ViewResolver 등록
+    }
+
+    // 📍 Servlet 3.0 파일 업로드 설정
+    @Bean
+    public MultipartResolver multipartResolver() {
+        StandardServletMultipartResolver resolver =
+                new StandardServletMultipartResolver();
+        return resolver;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer servletPropertyConfig() throws IOException, IOException {
+        return RootConfig.propertyConfig();
+    }
+}
