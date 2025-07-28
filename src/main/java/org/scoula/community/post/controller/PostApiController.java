f03b8e1625eba019dd220456fa30cc0a7d6327b1
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-07-28 13:29:18 +0900

    feat: 게시글 스크랩 CRUD 구현

--- src/main/java/org/scoula/community/post/controller/PostApiController.java
+++ src/main/java/org/scoula/community/post/controller/PostApiController.java
@@ -17,19 +17,14 @@ import org.scoula.community.post.dto.PostUpdateRequestDTO;
 import org.scoula.community.post.service.PostService;
 import org.scoula.response.ApiResponse;
 import org.scoula.response.ResponseCode;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/posts")
