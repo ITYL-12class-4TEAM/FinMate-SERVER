eda95c84b51f32fa18357523e43b5b89c1649a6e
이주영 <ptljoo1572@gmail.com>
2025-07-30 17:25:57 +0900

    fix: 타입 불일치 해결

--- src/main/java/org/scoula/preinfo/dto/PreInfoViewDTO.java
+++ src/main/java/org/scoula/preinfo/dto/PreInfoViewDTO.java
@@ -3,6 +3,8 @@ package org.scoula.preinfo.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import org.scoula.preinfo.enums.InvestmentCapacity;
+import org.scoula.preinfo.enums.InvestmentType;
 import org.scoula.wmti.enums.RiskPreference;
 
 import java.time.LocalDateTime;
@@ -45,13 +47,13 @@ public class PreInfoViewDTO {
     private Integer financialHealthScore;
 
     @ApiModelProperty(value = "투자 여력", example = "보통")
-    private String investmentCapacity;
+    private InvestmentCapacity investmentCapacity;
 
     @ApiModelProperty(value = "추천 월 투자금액", example = "500000")
     private Long recommendedMonthlyInvestment;
 
     @ApiModelProperty(value = "도출된 투자자 유형", example = "소극적 실용가형")
-    private String resultType;
+    private InvestmentType resultType;
 
     @ApiModelProperty(value = "위험 성향", example = "위험중립형")
     private RiskPreference riskPreference;
